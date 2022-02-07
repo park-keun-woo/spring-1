a49240fff00be58da092ae8c9f274283e963f7d8
JIEUN <JIEUN@DESKTOP-Q2SO514>
2022-02-07 10:51:28 +0900

    Initial upload

---
+++ myboot01/src/main/java/com/multi/myboot01/HelloController.java
@@ -0,0 +1,15 @@
+package com.multi.myboot01;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+	
+	@ResponseBody
+	@RequestMapping("/")
+	public String home() {
+		return "hello boot!";
+	}
+}
